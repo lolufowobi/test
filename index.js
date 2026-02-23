f5914c823beca266d0ca464f8e3ee4546679d2e9
Lawal Olufowobi <c_lawal.olufowobi@viasat.com>
2026-02-23 10:56:46 -0500

    cat work

---
+++ index.js
@@ -0,0 +1,37 @@
+//This is a silly tool that returns a random cat name from a list of cat names. It is meant to be used as a fun way to generate cat names for people who are looking for inspiration.
+//it uses github action lib to output the catName as an output variable that can be used in other steps of the workflow.
+import * as core from "@actions/core";
+
+const catNames = [
+  "Whiskers",
+  "Fluffy",
+  "Mittens",
+  "Shadow",
+  "Simba",
+  "Luna",
+  "Oliver",
+  "Chloe",
+  "Charlie",
+  "Bella",
+  "Max",
+  "Sophie",
+  "Tiger",
+  "Cleo",
+  "Smokey",
+];
+
+function getRandomCatName() {
+  const randomIndex = Math.floor(Math.random() * catNames.length);
+  return catNames[randomIndex];
+}
+
+try {
+  const catType = core.getInput("catType");
+  if (!catType) {
+    throw new Error("catType input is required");
+  }
+  const catName = getRandomCatName();
+  core.setOutput("catName", catName+" the "+catType);
+} catch (error) {
+  core.setFailed(error.message);
+}
