ae0617d7fd58c599032ad947f7a6f8bee5ed6a29
Lawal Olufowobi <c_lawal.olufowobi@viasat.com>
2026-05-22 12:02:45 -0400

    stop using cat

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
-//This is a silly tool that returns a random cat name from a list of cat names. It is meant to be used as a fun way to generate cat names for people who are looking for inspiration.
-//it uses github action lib to output the catName as an output variable that can be used in other steps of the workflow.
+//This is a silly tool that returns a random deployment name from a list of deployment names. It is meant to be used as a fun way to generate deployment names for people who are looking for inspiration.
+//it uses github action lib to output the deploymentName as an output variable that can be used in other steps of the workflow.
 import * as core from "@actions/core";
 
-const catNames = [
+const deploymentNames = [
   "Whiskers",
   "Fluffy",
   "Mittens",
@@ -21,21 +21,21 @@ const catNames = [
   "Smokey",
 ];
 
-function getRandomCatName() {
-  const randomIndex = Math.floor(Math.random() * catNames.length);
-  return catNames[randomIndex];
+function getRandomDeploymentName() {
+  const randomIndex = Math.floor(Math.random() * deploymentNames.length);
+  return deploymentNames[randomIndex];
 }
 
 try {
-  const catType = core.getInput("catType");
+  const deploymentType = core.getInput("deploymentType");
   const path = core.getInput("path");
   console.log("path", path);
-  if (!catType) {
-    throw new Error("catType input is required");
+  if (!deploymentType) {
+    throw new Error("deploymentType input is required");
   }
-  console.log("running for catType", catType);
-  const catName = getRandomCatName();
-  core.setOutput("catName", catName+" the "+catType);
+  console.log("running for deploymentType", deploymentType);
+  const deploymentName = getRandomDeploymentName();
+  core.setOutput("deploymentName", deploymentName+" the "+deploymentType);
 } catch (error) {
   core.setFailed(error.message);
 }
